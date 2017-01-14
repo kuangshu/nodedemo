c581ed8e8da129af2e9b8a5462d6d083f74a9b98
kuangshu <252019869@qq.com>
2017-01-15 00:31:27 +0800

    获取openid
    完成getopenid

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-'use strict';
+//require('babel-polyfill')
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -7,6 +8,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var request = require('superagent');
 //import request from 'superagent';
+var index = require('./routes/index');
 
 var app = express();
 
@@ -15,32 +17,50 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 var appId = 'wxa96bac0a8c19a07b';
-var appSecret = '041d03b9231abc652173abd465157eee';
+var appSecret = '1194d311c3ed40773cfc2bebd47c0aa4';
 
+function getOpenid(code){
+	return new Promise((resolve, reject)=>{
+		request
+			.get('https://api.weixin.qq.com/sns/oauth2/access_token')
+			.accept('json')
+			//.header('Accept', 'application/json')
+			.query({
+				appid: appId,
+				secret: appSecret,
+				code: code,
+				grant_type: 'authorization_code'
+			})
+			.then((err, res) => {
+				console.log(err.res.text, res)
+				err ? resolve(err.res.text) : reject(res);
+			});		
+	})
+
+}
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/node/wx/api/oauth2',function(req, res){
-	let code = req.query.code;
-	let getopenid = new Promise((resolve, reject)=>{
-		return request.post(`https://api.weixin.qq.com/sns/oauth2/access_token?appid=${appId}&secret=${appSecret}&code=${code}&grant_type=authorization_code`)
-			.end((err, res) => {
-				if(!err){
-					console.log(res)
-					resolve(res.test);
-				}else{
-					reject(err);
-				}
-			});
-	})
-	getopenid.then(data=>{
-		res.json(data);
-	})
+app.get('/node/wx/api/oauth2', async function(req, res){
+	var code = req.query.code;
+	var data = '';
+	await getOpenid(code).then(result => {
+		data = result;
+	}).catch(result=>{
+		data = result;
+	});
+	data = JSON.parse(data);
+	console.log(data);
+	if (data.openid) {
+		res.send(data.openid);
+	} else {
+		res.send(data.errmsg);
+	}
 })
 
 // catch 404 and forward to error handler
